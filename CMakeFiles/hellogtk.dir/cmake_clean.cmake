460f0d313c10b38765e56e9689abe70da5662827
Sergey Mikhno <sergey.mikhno@gmail.com>
2020-10-24 17:58:00 +0200

    initial commit

---
+++ CMakeFiles/hellogtk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellogtk.dir/testapp.o"
+  "hellogtk"
+  "hellogtk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hellogtk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ testapp.c
@@ -0,0 +1,36 @@
+#include <gtk/gtk.h>
+
+
+static void destroy(GtkWidget *window, GtkLabel *label) {
+    gtk_main_quit();
+}
+
+static gboolean delete_event(GtkWidget *window, GdkEvent *event, gpointer data) {
+    return FALSE;
+}
+
+int main(int argc, char *argv[]){
+    GtkWidget *window, *label;
+
+    gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Hello World!");
+    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+    gtk_widget_set_size_request(window, 200,100);
+
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(destroy), NULL);
+    g_signal_connect(G_OBJECT(window), "delete_event", G_CALLBACK(delete_event), NULL);
+
+    label = gtk_label_new("Hello World label");
+    gtk_label_set_selectable(GTK_LABEL(label), FALSE);
+
+    gtk_container_add(GTK_CONTAINER(window), label);
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
+    return 0;
+}
+
+
