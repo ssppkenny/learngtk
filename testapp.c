8b1751ce15ee63e9a41cbf8a75b262efcb67814f
Sergey Mikhno <sergey.mikhno@gmail.com>
2020-10-25 19:09:59 +0100

    get window title

--- testapp.c
+++ testapp.c
@@ -1,5 +1,14 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
 
+
+static void key_press(GtkWidget *window,  gpointer data){
+    char *string = NULL;
+    printf("clicked\n");
+    GtkWidget* w = data;
+    g_object_get(G_OBJECT(w), "title", &string, NULL);
+    printf("%s\n", string);
+}
 
 static void destroy(GtkWidget *window, GtkLabel *label) {
     gtk_main_quit();
@@ -10,7 +19,7 @@ static gboolean delete_event(GtkWidget *window, GdkEvent *event, gpointer data) 
 }
 
 int main(int argc, char *argv[]){
-    GtkWidget *window, *label;
+    GtkWidget *window, *label, *button;
 
     gtk_init(&argc, &argv);
 
@@ -18,14 +27,24 @@ int main(int argc, char *argv[]){
     gtk_window_set_title(GTK_WINDOW(window), "Hello World!");
     gtk_container_set_border_width(GTK_CONTAINER(window), 10);
     gtk_widget_set_size_request(window, 200,100);
+    gtk_window_set_resizable((GtkWindow*)window, FALSE);
+
+    button = gtk_button_new_with_label("My Button");
+    gtk_container_add(GTK_CONTAINER(window), button);
+    int n = 100;
+
+    
+    //g_object_set(window, "title", "MyTitle", NULL);
 
     g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(destroy), NULL);
     g_signal_connect(G_OBJECT(window), "delete_event", G_CALLBACK(delete_event), NULL);
+    g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(key_press), window);
 
-    label = gtk_label_new("Hello World label");
-    gtk_label_set_selectable(GTK_LABEL(label), FALSE);
+    //label = gtk_label_new("Hello World label");
+    //gtk_label_set_selectable(GTK_LABEL(label), FALSE);
 
-    gtk_container_add(GTK_CONTAINER(window), label);
+    //gtk_container_add(GTK_CONTAINER(window), label);
+
 
     gtk_widget_show_all(window);
 
